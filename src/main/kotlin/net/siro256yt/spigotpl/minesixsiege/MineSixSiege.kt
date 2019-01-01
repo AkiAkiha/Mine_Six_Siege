8409c8d190be988313688f3e1930c2704bf24d98
Siro_256 <siro256@outlook.jp>
2019-01-02 05:49:34 +0900

    とりあえず初期設定書いた

---
+++ src/main/kotlin/net/siro256yt/spigotpl/minesixsiege/MineSixSiege.kt
@@ -0,0 +1,16 @@
+package net.siro256yt.spigotpl.minesixsiege
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MineSixSiege: JavaPlugin() {
+    //@TODO スタート時の処理とか全部書く(ファイル作っただけ)
+
+
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
